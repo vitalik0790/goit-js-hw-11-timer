5de3e08ddf16723abc84f4e5db8ddc783c69aea7
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-12-09 18:43:06 +0200

    timer-11dz

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jan 01, 2021'),
-// });
 
-const refs = {
-    clockDays: document.querySelector('span[data-value="days"]'),
-    clockHours: document.querySelector('span[data-value="hours"]'),
-    clockMinutes: document.querySelector('span[data-value="mins"]'),
-    clockSecs: document.querySelector('span[data-value="secs"]'),
-}
+class CountDownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+        this.timerId = document.querySelector(this.selector);
+        this.clockDays = this.timerId.querySelector('span[data-value="days"]');
+        this.clockHours = this.timerId.querySelector('span[data-value="hours"]');
+        this.clockMinutes = this.timerId.querySelector('span[data-value="mins"]');
+        this.clockSecs = this.timerId.querySelector('span[data-value="secs"]');
+        this.timer();
+    }
 
-const timer = {
-    start() {
-        const targetDate = new Date('Jan 01, 2021');
-
-
+    timer() {
         setInterval(() => {
             const currentTime = Date.now();
-            const deltaTime = targetDate - currentTime
-            updateClockface(deltaTime)
+            const time = this.targetDate - currentTime;
+
+            const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+            this.clockDays.textContent = days;
+
+            const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+            this.clockHours.textContent = hours;
+
+            const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+            this.clockMinutes.textContent = mins;
+
+            const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+            this.clockSecs.textContent = secs;
         }, 1000);
-    },
+    };
+
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
 };
-timer.start();
 
-function updateClockface(time) {
-    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    refs.clockDays.textContent = days;
+const countDown = new CountDownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jan 01, 2021'),
+});
 
-    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    refs.clockHours.textContent = hours;
-
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    refs.clockMinutes.textContent = mins;
-
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-    refs.clockSecs.textContent = secs;
-}
-
-function pad(value) {
-    return String(value).padStart(2, '0');
-}
